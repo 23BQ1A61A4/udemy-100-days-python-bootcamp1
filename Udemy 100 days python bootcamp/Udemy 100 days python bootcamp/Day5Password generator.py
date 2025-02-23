ab9e6d15a694c96a32e5ce6e8c5a46c4e00f0662
PATTEM DOLLY SAI ISWARYA <pdsiswarya@gmail.com>
2025-02-24 04:54:20 +0530

    Update Day5Password generator.py

--- Udemy 100 days python bootcamp/Udemy 100 days python bootcamp/Day5Password generator.py
+++ Udemy 100 days python bootcamp/Udemy 100 days python bootcamp/Day5Password generator.py
@@ -2,14 +2,15 @@ import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
+#It is a python program to generate the password
+#take the inpuut from the user, that how many letters , symbols and numbers should be there in the password
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 
-
+#design the password according to the user input
 # Hard level
 password_list = []
 for char in range(0, nr_letters):
@@ -20,7 +21,7 @@ for char in range(0, nr_symbols):
 
 for char in range(0, nr_numbers):
     password_list.append(random.choice(numbers))
-
+#Print the password list
 print(password_list)
 random.shuffle(password_list)
 print(password_list)
@@ -28,6 +29,6 @@ print(password_list)
 password = ""
 for char in password_list:
     password += char
-
+#print the password
 print(f"Your password is: {password}")
 
