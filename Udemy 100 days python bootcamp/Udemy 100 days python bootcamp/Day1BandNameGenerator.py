19a6f64c41bb251361c9d51750ba69ea5ef1c6b6
PATTEM DOLLY SAI ISWARYA <pdsiswarya@gmail.com>
2025-02-24 04:48:58 +0530

    Update Day1BandNameGenerator.py

--- Udemy 100 days python bootcamp/Udemy 100 days python bootcamp/Day1BandNameGenerator.py
+++ Udemy 100 days python bootcamp/Udemy 100 days python bootcamp/Day1BandNameGenerator.py
@@ -1,6 +1,9 @@
 # 5. Band Name Generator (A simple project often introduced on Day 1)
-
+#we are generating our own band name
 print("Welcome to the Band Name Generator.")
+#giving city name as input
 city = input("What's the name of the city you grew up in?\n")
+#giving pet name as input
 pet = input("What's your pet's name?\n")
+#combining city name and pet name to create band name
 print("Your band name could be " + city + " " + pet)
